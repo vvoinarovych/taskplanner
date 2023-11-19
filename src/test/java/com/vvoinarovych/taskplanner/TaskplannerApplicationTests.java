bc05ac57fc6c135acdadf266153177e37da166d5
vvoinarovych <vvoinarovych@gmail.com>
2023-11-19 22:28:44 +0100

    Initial commit

---
+++ src/test/java/com/vvoinarovych/taskplanner/TaskplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvoinarovych.taskplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
